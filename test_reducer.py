bef6c2c4b018a493c0903fbad50fa4e7188978dc
kevinortiz1053 <131682694+kevinortiz1053@users.noreply.github.com>
2023-10-10 23:51:17 -0400

    Add files via upload

---
+++ test_reducer.py
@@ -0,0 +1,22 @@
+"""reducer.py"""
+import sys
+
+
+def clean_text(text):
+    text = text.lower()
+    punc = "()'"
+    for char in text:
+        if char in punc:
+            text = text.replace(char, "")
+    return text
+
+# Given an array with all (president, word-valence) pairs
+
+d_grouping = {}
+for line in sys.stdin:
+    line = line.strip()
+    president, valence = line.split(',', 1)
+    president = clean_text(president)
+    valence = int(clean_text(valence))
+    pair = (president, valence)
+    print(pair)
